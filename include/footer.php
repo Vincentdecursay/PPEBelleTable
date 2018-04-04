0c0984cfbc6924ef7746cdb2ce0420ee250c7f1c
Remitekky <31990166+Remitekky@users.noreply.github.com>
2018-04-04 11:45:54 +0200

    Structure de base bootstrap
    Début du projet PPE Belle table

---
+++ include/footer.php
@@ -0,0 +1,23 @@
+<footer>
+	<div>
+		<ul>
+			<li>Infos légales</li>
+			<li>Contact</li>
+			<li>Horaires</li>
+			<li>Offre d'emplois</li>
+		</ul>
+	</div>
+	<div>
+		<ul>
+			<li>Structure informatique</li>
+			<li>Matériel informatique</li>
+		</ul>
+	</div>
+	<div>
+		<ul>
+			<li>fb</li>
+			<li>instagram</li>
+			<li>Twiter</li>
+		</ul>
+	</div>
+</footer>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Belle Table</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<div class="container">
+		<header class="row">
+			<div class="col-lg-12"><!--12colonnes-->
+				<div class="row">
+					<a href="index.php" title="accueil"><img src="img/logo.jpg" alt="logo" title="logo"/></a>
+					<h1>Slogan !</h1>
+					
+					<nav class="navbar navbar-default navbar-fixed-top">
+						<ul class="nav navbar-nav">
+							<li><a href="presentation.html">Présentation</a></li>
+							<li><a href="reservation.html">Réservation</a></li>
+							<li><a href="avis.html">Avis</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+
+
+
+
+
+
+
+
+		<footer>
+			<footer>
+				<div>
+					<ul>
+						<li>Infos légales</li>
+						<li>Contact</li>
+						<li>Horaires</li>
+						<li>Offre d'emplois</li>
+					</ul>
+				</div>
+				<div>
+					<ul>
+						<li>Structure informatique</li>
+						<li>Matériel informatique</li>
+					</ul>
+				</div>
+				<div>
+					<ul>
+						<li>fb</li>
+						<li>instagram</li>
+						<li>Twiter</li>
+					</ul>
+				</div>
+			</footer>
+		</footer>
+
+
+
+
+	</div>
+</body>
+</html>
